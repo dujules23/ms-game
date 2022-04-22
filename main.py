55e8fc2bad0b401b68d42c1003aa3fe5d8ace2b4
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2022-04-22 11:59:03 -0400

    added left frame

--- main.py
+++ main.py
@@ -11,6 +11,15 @@ top_frame = Frame(
   width = 1440,
   height = 180
 )
+top_frame.place(x=0, y=0)
+
+left_frame = Frame(
+  root,
+  bg = "blue",
+  width = 360,
+  height = 540
+)
+left_frame.place(x=0, y=180)
 
 root.mainloop()
 
