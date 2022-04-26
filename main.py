83ecefe913cc53a5646c367d04faca7f8f3caa01
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2022-04-25 20:48:01 -0400

    added utils and settings for height, width, and percentages to avoid hard coding

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 from tkinter import *
 
+import settings
+import utils
+
 root = Tk()
-root.geometry('1440x720')
-root.title('Minesweeper Game')
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
+root.title('Minesweeper')
 root.resizable(False, False)
 
 top_frame = Frame(
   root,
-  bg = 'red',
-  width = 1440,
-  height = 180
+  bg = 'red', 
+  width = settings.WIDTH,
+  height = utils.height_prct(25)
 )
 top_frame.place(x=0, y=0)
 
 left_frame = Frame(
   root,
   bg = "blue",
-  width = 360,
-  height = 540
+  width = utils.width_prct(25),
+  height = utils.height_prct(75)
 )
-left_frame.place(x=0, y=180)
+left_frame.place(x=0, y=utils.height_prct(25))
 
 root.mainloop()
 
---
+++ utils.py
@@ -0,0 +1,13 @@
+import settings
+
+
+def height_prct(percentage):
+  return (settings.HEIGHT / 100) * percentage
+
+def width_prct(percentage):
+  return (settings.WIDTH / 100) * percentage
+
+
+# print(height_prct(25))
+
+print(width_prct(25))
