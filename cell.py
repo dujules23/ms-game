900b2226684209a2c574a5460e23af3d6a924c74
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2022-04-28 11:10:19 -0400

    added x and y to init function in cell.py, included the attributes in the Cell method in main.py

--- cell.py
+++ cell.py
@@ -1,16 +1,18 @@
 from tkinter import Button
 
 class Cell:
-  def __init__(self, is_mine = False):
+  def __init__(self, x, y, is_mine = False):
       self.is_mine = is_mine 
       self.cell_btn_object = None
+      self.x = x
+      self.y = y
 
   def create_btn_object(self, location):
     btn = Button(
       location,
       width = 12,
       height = 4,
-      text = 'Text'
+      text = f"{self.x, self.y}"
     )
     btn.bind('<Button-1>', self.left_click_actions ) # Left click convention
     btn.bind('<Button-2>', self.right_click_actions ) # Right click convention
