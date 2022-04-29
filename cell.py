c23e0da2f641624094cb72158ffb8719cc49228e
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2022-04-29 10:03:58 -0400

    created method to check and display how many mines are around the clicked cell, seems to be working properly

--- cell.py
+++ cell.py
@@ -18,7 +18,6 @@ class Cell:
       location,
       width = 12,
       height = 4,
-      text= f"{self.x, self.y}"
     )
     btn.bind('<Button-1>', self.left_click_actions ) # Left click convention
     btn.bind('<Button-2>', self.right_click_actions ) # Right click convention
@@ -36,8 +35,9 @@ class Cell:
       if cell.x == x and cell.y == y:
         return cell
 
-  def show_cell(self):
-    surrounded_cells = [
+  @property
+  def surrounded_cells(self):
+    cells = [
       self.get_cell_by_axis(self.x - 1, self.y -1),
       self.get_cell_by_axis(self.x - 1, self.y),
       self.get_cell_by_axis(self.x - 1, self.y + 1),
@@ -47,7 +47,22 @@ class Cell:
       self.get_cell_by_axis(self.x + 1, self.y + 1),
       self.get_cell_by_axis(self.x, self.y + 1)
     ]
-    print(surrounded_cells)
+    
+    # List Comprehension
+    cells = [cell for cell in cells if cell is not None]
+    return cells
+  
+  @property
+  def surrounded_cells_mines_length(self):
+    counter = 0
+    for cell in self.surrounded_cells:
+      if cell.is_mine:
+        counter += 1
+
+    return counter  
+  
+  def show_cell(self):
+    self.cell_btn_object.configure(text = self.surrounded_cells_mines_length)
 
   def show_mine(self):
     # Logic to interrupt the game and display a message that player lost! 
